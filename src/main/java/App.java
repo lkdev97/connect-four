ea3806e19efd41dff20a2a9e766479ff421fd87f
Felix Schopen <felix.schopen@mni.thm.de>
2019-05-07 20:30:48 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,5 +6,16 @@ public class App {
         server.enableStaticFiles("/public");
 
         server.start(80);
+        
+        Game g1 = new Game("1111", "2222");
+        g1.put(0);
+        g1.put(0);
+        g1.put(6);
+        g1.put(7);
+        g1.put(7);
+        g1.put(7);
+        g1.put(7);
+        g1.printField();
+        
     }
 }
