603d086dca48c1e8a53612cead3d2e5c0a021144
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-07 22:45:42 +0200

    Add dummy page with style and scripts

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('JS file loaded.');
+    let createGameButton = document.getElementById('create-game-button');
+    let joinGameButton = document.getElementById('join-game-button');
+    let joinGameId = document.getElementById('join-game-code');
+
+
+    if (createGameButton && joinGameButton && joinGameId) {
+        createGameButton.addEventListener('click', createNewGame);
+        joinGameButton.addEventListener('click', () => joinGame(joinGameId.value));
+        console.log('All UI elements registered.');
+    }
+
+
+
+    function createNewGame() {
+        alert('Creating new game...');
+    }
+    function joinGame(gameId) {
+        if (gameId)
+            alert(`Joining game with id "${gameId}"...`);
+    }
 });
