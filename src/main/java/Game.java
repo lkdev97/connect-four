ecf1dff38d0d774522a7c15bd9bf5185b8813ce7
Felix Schopen <felix.schopen@mni.thm.de>
2019-05-07 19:01:58 +0200

    Delete Game.java

--- src/main/java/Game.java
+++
@@ -1,7 +0,0 @@
-public interface Game {
-
-    boolean put();  //Spieler macht Zug, returns false wenn Zug ungültig
-    void undo();    //Letzer (oder letzen zwei Züge) werden Rückgängig gemacht
-    void newGame(); //Undo alle Züge oder erstellt neues Spiel mit neuen Spielern
-
-}
