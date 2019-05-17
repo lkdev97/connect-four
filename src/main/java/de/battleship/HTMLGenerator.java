54823131998ef36a6568c5b3c73673d5be011d1d
Marlon Drolsbach <marlondrolsbach@pool-10-192-48-32.dyn.thm.de>
2019-05-17 11:03:05 +0200

    toString w/ Html

--- src/main/java/de/battleship/HTMLGenerator.java
+++ src/main/java/de/battleship/HTMLGenerator.java
@@ -5,13 +5,14 @@ public final class HTMLGenerator {
         StringBuilder boardString = new StringBuilder();
         for (int y = 0; y < content.length; y++)
             for (int x = 0; x < content[y].length; x++)
-                boardString.append(generateCellElement(content[y][x]));
+                boardString.append(generateCellElement(content[x][y]));
 
         return boardString.toString();
     }
 
-
     private static String generateCellElement(int playerId) {
-        return "<div class=\"box" + (playerId == 2 ? " red-ball" : playerId == 1 ? " yellow-ball" : playerId == 3 ? " green-ball" : "") + "\"></div>";
+        return "<div class=\"box"
+                + (playerId == 2 ? " red-ball" : playerId == 1 ? " yellow-ball" : playerId == 3 ? " green-ball" : "")
+                + "\"></div>\n";
     }
 }
