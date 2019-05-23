50c09fd65302d013611f9be890191f0a5f1ed113
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-23 14:53:08 +0200

    Move ui function to ui.js

--- src/main/resources/public/js/connection.js
+++ src/main/resources/public/js/connection.js
@@ -57,9 +57,7 @@ function fetchGameList() {
     sendToServer('/gamelist', {})
         .then((response) => {
             if (response && response.games) {
-                // alle vorhandenen Einträge löschen
-                while (lobbyBrowser.firstChild)
-                    lobbyBrowser.firstChild.remove();
+                clearGameBrowser();
 
                 // neue Einträge einfügen
                 for (let gameId of response.games)
