cba4de8195dfb8be9bb8e5e9a0ca3bf7567e9ddd
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-24 01:04:36 +0200

    Make Player constructor public

--- src/main/java/de/battleship/Player.java
+++ src/main/java/de/battleship/Player.java
@@ -7,7 +7,7 @@ public class Player {
     boolean turn;
     int wins;
 
-    Player(String name) {
+    public Player(String name) {
         this.name = name; // habe mir überlegt, dass man in der HTML-Datei, die Spieler Namen eingeben
                           // lassen könnte und es wird angezeigt welcher Spieler dran ist
         wins = 0;
