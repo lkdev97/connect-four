68504ce24ad697b26caa02c094bf1041486c2dd5
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-24 03:16:36 +0200

    Fix player name null bug

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -14,7 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
         joinGameButton.addEventListener('click', () => joinGame(joinGameId.value));
         console.log('Alle UI Elemente gefunden und registriert.');
 
-        playerName = prompt("Bitte gib deinen Spielernamen ein:", `Player${Math.floor(Math.random() * 9001 + 1000)}`);
+        playerName = `Player${Math.floor(Math.random() * 9001 + 1000)}`;
+        playerName = prompt("Bitte gib deinen Spielernamen ein:", playerName) || playerName;
 
         fetchGameList();
         connectToGameListEvents();
