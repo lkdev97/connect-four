5360f044ff744f5859cbebc5b8cfdaa9bea5f7a7
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-24 03:44:15 +0200

    Remove Message web packet

--- src/main/java/de/battleship/server/packets/web/OutMessage.java
+++
@@ -1,9 +0,0 @@
-package de.battleship.server.packets.web;
-
-public class OutMessage extends WebPacket {
-    public String message;
-
-    public OutMessage(String message) {
-        this.message = message;
-    }
-}
