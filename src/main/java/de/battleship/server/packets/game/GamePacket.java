5f13e7a4c46889e36b9560faba8ae26e14239776
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-24 16:01:52 +0200

    Implement toString for all game packets

--- src/main/java/de/battleship/server/packets/game/GamePacket.java
+++ src/main/java/de/battleship/server/packets/game/GamePacket.java
@@ -2,11 +2,19 @@ package de.battleship.server.packets.game;
 
 import java.util.HashMap;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.javalin.json.JavalinJson;
+
 public abstract class GamePacket {
     public static HashMap<Integer, Class<? extends GamePacket>> registeredInPackets;
 
+    private static ObjectMapper jsonConverter;
+
 
     static {
+        jsonConverter = new ObjectMapper();
+
         registeredInPackets = new HashMap<>();
         registeredInPackets.put(0, InPingPacket.class);
         registeredInPackets.put(1, InConnectRequest.class);
@@ -15,5 +23,16 @@ public abstract class GamePacket {
 
     public static Class<? extends GamePacket> getInPacketById(int id) {
         return registeredInPackets.getOrDefault(id, null);
+    }
+
+
+    @Override
+    public String toString() {
+        try {
+            return jsonConverter.writeValueAsString(this);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
