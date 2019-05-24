6a0d42d64e6a0cac86df259f10a2c3207a9313dd
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-24 03:02:17 +0200

    Create Lobby.java

---
+++ src/main/java/de/battleship/Lobby.java
@@ -0,0 +1,26 @@
+package de.battleship;
+
+import java.util.ArrayList;
+
+import io.javalin.Javalin;
+
+public class Lobby {
+    private String lobbyId;
+
+    private ArrayList<Player> players;
+
+
+    public Lobby(String lobbyId, Javalin server) {
+        this.lobbyId = lobbyId;
+        this.players = new ArrayList<Player>();
+
+    }
+    
+
+    public boolean addPlayer(Player player) {
+        return true;
+    }
+    public void removePlayer(Player player) {
+        
+    }
+}
