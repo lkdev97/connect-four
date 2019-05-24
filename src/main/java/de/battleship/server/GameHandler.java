d29202c61bfd4d5bb686785236999b61216aff7c
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-24 18:02:43 +0200

    Improve GameHandler

--- src/main/java/de/battleship/server/GameHandler.java
+++ src/main/java/de/battleship/server/GameHandler.java
@@ -2,9 +2,11 @@ package de.battleship.server;
 
 import de.battleship.App;
 import de.battleship.Game;
+import de.battleship.server.packets.game.GamePacket;
 import de.battleship.server.packets.game.OutError;
 import de.battleship.server.packets.game.OutGameField;
 import io.javalin.Javalin;
+import io.javalin.websocket.WsSession;
 
 public class GameHandler {
     private Javalin server;
@@ -12,6 +14,7 @@ public class GameHandler {
 
     public GameHandler(Javalin server) {
         this.server = server;
+
         this.server.ws("/:game-id", ws -> {
             ws.onConnect(session -> {
                 Game game = App.getGameManager().getGameById(session.pathParam("game-id"));
@@ -21,17 +24,28 @@ public class GameHandler {
                     session.close(1, "Disconnect by server.");
                 }
             });
-
-            ws.onMessage((session, message) -> {
-                Game game = App.getGameManager().getGameById(session.pathParam("game-id"));
-                System.out.println(session.pathParam("game-id") + " >> " + message);
-
-                session.send(new OutGameField(game.toString()).toString());
-            });
-
+            ws.onMessage(this::handleClientMessage);
             ws.onClose((session, statusCode, reason) -> {
-                System.out.println("WebSocket closed for id " + session.pathParam("game-id") + ": (" + statusCode + ") " + reason);
+                // TODO: Remove player from lobby
+                System.out.println(
+                        "WebSocket closed for id " + session.pathParam("game-id") + ": (" + statusCode + ") " + reason);
             });
         });
+    }
+
+
+    private void handleClientMessage(WsSession session, String message) {
+        Game game = App.getGameManager().getGameById(session.pathParam("game-id"));
+                System.out.println(session.pathParam("game-id") + " >> " + message);
+
+                this.sendPacket(session, new OutGameField(game.toString()));
+    }
+    
+
+    /**
+     * Sendet ein Packet an den Client.
+     */
+    private void sendPacket(WsSession session, GamePacket packet) {
+        session.send(packet.toString());
     }
 }
