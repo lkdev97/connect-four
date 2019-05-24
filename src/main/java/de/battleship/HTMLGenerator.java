6a918e5d97064ebf77f3964a29db8af2744769d6
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-24 14:17:04 +0200

    Fix board html generation

--- src/main/java/de/battleship/HTMLGenerator.java
+++ src/main/java/de/battleship/HTMLGenerator.java
@@ -7,19 +7,21 @@ public class HTMLGenerator {
         for (int y = 0; y < content.length; y++) {
             boardString.append("<div class=\"board-row\">\n");
             for (int x = 0; x < content[y].length; x++)
-                boardString.append(generateCellElement(content[x][y]));
+                boardString.append(generateCellElement(x, content[x][y]));
             boardString.append("</div>\n");
         }
 
         return boardString.toString();
     }
 
-    private static String generateCellElement(int playerId) {
+    private static String generateCellElement(int cellNum, int playerId) {
         return "\t<div class=\"box"
                 + (playerId == 1 ? " yellow-ball"
                         : playerId == 2 ? " red-ball"
                                 : playerId == 3 ? " yellow-ball winner" 
                                     : playerId == 4 ? " red-ball winner" : "")
-                + "\"></div>\n";
+                + "\">"
+                +   "<button class=\"btn btn-light\" type=\"button\" onclick=\"sendMove(" + cellNum + ")\"></button>" 
+                + "</div>\n";
     }
 }
