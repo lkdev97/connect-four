d92332093a48dc68f92cb2c3b620545fb9627e52
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-25 04:46:02 +0200

    Handle player move packet

--- src/main/java/de/battleship/server/packets/game/InPlayerMove.java
+++ src/main/java/de/battleship/server/packets/game/InPlayerMove.java
@@ -1,5 +1,15 @@
 package de.battleship.server.packets.game;
 
+import de.battleship.Game;
+import de.battleship.server.GameHandler;
+import io.javalin.websocket.WsSession;
+
 public class InPlayerMove extends GamePacket {
     public int column;
+
+    @Override
+    public void handle(GameHandler gameHandler, WsSession session, Game game) {
+        game.makeTurn(this.column);
+        gameHandler.sendPacket(session, new OutGameField(game.toString()));
+    }
 }
