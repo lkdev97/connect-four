03d96bfbdb4d0b17533652c810e4d6cb0ceaf20d
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-25 09:28:50 +0200

    Implement connect success packet + improve connect request packet

--- src/main/java/de/battleship/server/packets/game/InConnectRequest.java
+++ src/main/java/de/battleship/server/packets/game/InConnectRequest.java
@@ -10,19 +10,15 @@ public class InConnectRequest extends GamePacket {
 
     @Override
     public void handle(GameHandler gameHandler, WsSession session, Lobby lobby, Player player) {
-        if (lobby.getGame() != null) {
-            if (lobby.getPlayersAmount() < lobby.getMaxPlayersAmount()) {
-                player = new Player(this.playerName);
-                gameHandler.addConnectedPlayer(session, player);
+        if (lobby.getPlayersAmount() < lobby.getMaxPlayersAmount()) {
+            player = new Player(this.playerName);
+            gameHandler.addConnectedPlayer(session, player);
 
-                if (lobby.addPlayer(player))
-                    gameHandler.sendPacket(session, new OutGameField(lobby.getGame().toString()));
-                else
-                    gameHandler.sendErrorMessage(session, "Couldn't add you to lobby. Try changing your name.", true);
-            }
+            if (lobby.addPlayer(player))
+                gameHandler.sendPacket(session, new OutGameField(lobby.getGame().toString()));
             else
-                gameHandler.sendErrorMessage(session, "Lobby is already full.", true);
-
-        }
+                gameHandler.sendErrorMessage(session, "Couldn't add you to lobby. Try changing your name.", true);
+        } else
+            gameHandler.sendErrorMessage(session, "Lobby is already full.", true);
     }
 }
