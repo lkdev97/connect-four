926ef528fa38e8f5b3e2a48cc8101bee3ecd859a
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-25 04:44:06 +0200

    Handle connect request packet

--- src/main/java/de/battleship/server/packets/game/InConnectRequest.java
+++ src/main/java/de/battleship/server/packets/game/InConnectRequest.java
@@ -1,5 +1,14 @@
 package de.battleship.server.packets.game;
 
+import de.battleship.Game;
+import de.battleship.server.GameHandler;
+import io.javalin.websocket.WsSession;
+
 public class InConnectRequest extends GamePacket {
     public String playerName;
+
+    @Override
+    public void handle(GameHandler gameHandler, WsSession session, Game game) {
+        gameHandler.sendPacket(session, new OutGameField(game.toString()));
+    }
 }
