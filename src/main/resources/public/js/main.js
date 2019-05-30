97e019096c6612822eb4b4cc51e296d80a9a3f1d
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-30 23:37:06 +0200

    Join game when url hash changes

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -14,7 +14,14 @@ document.addEventListener('DOMContentLoaded', () => {
         txtUserName.addEventListener('click', changeUserName);
         joinGameButton.addEventListener('click', () => joinGame(joinGameId.value));
         leaveGameButton.addEventListener('click', disconnectFromGame);
-        window.addEventListener('hashchange', () => joinGameId.value = window.location.hash);
+        window.addEventListener('hashchange', () => {
+            if (window.location.hash.length > 1) {
+                joinGameId.value = window.location.hash;
+
+                if (!isConnectedToGame())
+                    joinGame(location.hash.substr(1));
+            }
+        });
         console.log('Alle UI Elemente gefunden und registriert.');
 
         fetchGameList();
@@ -23,9 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
         setUserName(`Player${Math.floor(Math.random() * 9001 + 1000)}`);
         setUserName(prompt("Bitte gib deinen Spielernamen ein:", playerName) || playerName);
 
-        if (location.hash.length > 0) {
+        if (location.hash.length > 1)
             joinGame(location.hash.substr(1));
-        }
     }
     else
         console.error('Manche UI Elemente fehlen.');
