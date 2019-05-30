7fef0612a5fc567b814b3421852f3b24f74ece1a
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-30 21:18:00 +0200

    Add more functions and comments

--- src/main/java/de/battleship/OnlinePlayer.java
+++ src/main/java/de/battleship/OnlinePlayer.java
@@ -2,7 +2,13 @@ package de.battleship;
 
 import io.javalin.websocket.WsSession;
 
+/**
+ * Stellt einen zum Server verbundenen Spieler dar.
+ */
 public class OnlinePlayer extends Player {
+    /**
+     * Speichert die WebSocket-Session des Spielers.
+     */
     private WsSession session;
 
 
@@ -12,15 +18,31 @@ public class OnlinePlayer extends Player {
     }
 
 
+    /**
+     * Trennt die Verbindung zum Spieler.
+     */
     public void disconnect() {
         this.disconnect(null);
     }
+    /**
+     * Trennt die Verbindung zum Spieler mit einem angegebenen Grund.
+     */
     public void disconnect(String reason) {
-        if (this.session.isOpen())
-            this.session.close();
+        if (this.isConnected())
+            this.session.close(1, reason);
     }
 
 
+    /**
+     * Sagt aus, ob der Spieler mit dem Server verbunden ist.
+     */
+    public boolean isConnected() {
+        return this.session.isOpen();
+    }
+
+    /**
+     * Gibt die WebSocket-Session des Spielers zurück.
+     */
     public WsSession getSession() {
         return this.session;
     }
