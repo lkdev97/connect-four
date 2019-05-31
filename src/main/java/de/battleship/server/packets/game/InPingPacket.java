3f47a83d8735b9e06299bbb7b903510eb158a836
Alexander Pantelkin <alexander.pantelkin@mni.thm.de>
2019-05-31 14:36:57 +0200

    Implement handle method

--- src/main/java/de/battleship/server/packets/game/InPingPacket.java
+++ src/main/java/de/battleship/server/packets/game/InPingPacket.java
@@ -1,5 +1,13 @@
 package de.battleship.server.packets.game;
 
+import de.battleship.Lobby;
+import de.battleship.OnlinePlayer;
+import de.battleship.server.GameHandler;
+import io.javalin.websocket.WsSession;
+
 public class InPingPacket extends GamePacket {
-    
+    @Override
+    public void handle(GameHandler gameHandler, WsSession session, Lobby lobby, OnlinePlayer player) {
+        
+    }
 }
